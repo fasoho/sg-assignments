d13db6ae46a8437e45fd924482483b35a362ed7e
JP <1.4.gigz@gmail.com>
2019-04-29 14:01:28 -0500

    Add Practice exercises 01 and 02 in Milestone 01

---
+++ javaOOP/src/com/sg/milestone01/Milestone01PracticeProgrammingExercises.java
@@ -0,0 +1,32 @@
+package com.sg.milestone01;
+
+import com.sg.milestone01.exercises.practice01_Basics.M01BasicsExercises;
+import com.sg.milestone01.exercises.practice02_Variables.M02BasicsExercises;
+
+/**
+ * @project javaOOP
+ * @user jp
+ * @date 2019-04-29
+ */
+public class Milestone01PracticeProgrammingExercises {
+
+  public static void main(String[] args) {
+
+    M01BasicsExercises m01BasicsExercises = new M01BasicsExercises();
+    M02BasicsExercises m02BasicsExercises = new M02BasicsExercises();
+
+    m01BasicsExercises.ABeginning();
+    m01BasicsExercises.ProjectGutenberg();
+    m01BasicsExercises.CommentingCode();
+    m01BasicsExercises.AllTheMaths();
+
+    m02BasicsExercises.InABucket();
+    m02BasicsExercises.MoreBucketsMoreFun();
+    m02BasicsExercises.TheOrderOfThings();
+    m02BasicsExercises.BestAdderEver();
+    m02BasicsExercises.AllAboutMe();
+    m02BasicsExercises.MenuOfChampions();
+
+  }
+
+}
